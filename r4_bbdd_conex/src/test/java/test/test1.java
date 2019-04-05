b52b23af5060f15c16c9d1b92f23543c4278bc89
IN1DM3B_18 <IN1DM3B_18@H015112.NET>
2019-04-05 12:51:02 +0200

    error de libreria

---
+++ r4_bbdd_conex/src/test/java/test/test1.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test1 {
+
+	@Test
+	public final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
